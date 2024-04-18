3fa0e702e7037fba50942ed4950646110e41fdea
MrSp000ky <miguelcamacho241@outlook.com>
2024-04-17 20:07:58 -0700

    Commit tarea

---
+++ lib/presentation/providers/providers.dart
@@ -0,0 +1,6 @@
+export 'movies/movies_providers.dart';
+export 'movies/movies_repository_provider.dart';
+export 'package:proyecto_profe/presentation/providers/actors/actors_by_movie_provider.dart';
+export 'package:proyecto_profe/presentation/providers/movies/movie_info_provider.dart';
+export 'package:proyecto_profe/presentation/providers/storage/favorite_movies_provider.dart';
+export 'package:proyecto_profe/presentation/providers/storage/local_storage_provider.dart';
---
+++ lib/presentation/screens/views/viewsBarril.dart
@@ -0,0 +1,3 @@
+export 'package:proyecto_profe/presentation/screens/views/movies/favorites_view.dart';
+export 'package:proyecto_profe/presentation/screens/views/movies/home_view.dart';
+export 'package:proyecto_profe/presentation/screens/views/movies/popular_view.dart';
